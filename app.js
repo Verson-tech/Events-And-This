dcd161ba50421ee0a36ebe1a50e7bb3da1a9356e
vira.murray <vira.avramenko@gmail.com>
2022-01-06 17:34:27 +0200

    this

--- app.js
+++ app.js
@@ -9,15 +9,17 @@ const buttons = document.querySelectorAll("button");
 
 for (let button of buttons) {
   button.addEventListener("click", function () {
-    button.style.backgroundColor = makeRandColor();
-    button.style.color = makeRandColor();
+      console.log(this)
+    this.style.backgroundColor = makeRandColor();
+    this.style.color = makeRandColor();
   });
 }
 
 const h1s = document.querySelectorAll("h1");
 for (let h1 of h1s) {
   h1.addEventListener("click", function () {
-    h1.style.backgroundColor = makeRandColor();
-    h1.style.color = makeRandColor();
+      console.log(this)
+    this.style.backgroundColor = makeRandColor();
+    this.style.color = makeRandColor();
   });
 }
