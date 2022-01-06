98201fea189939a6056b738521d1ff0b73def4e5
vira.murray <vira.avramenko@gmail.com>
2022-01-06 17:21:08 +0200

    button.style.backgroundColor = makeRandColor();

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const buttons = document.querySelectorAll('button');
 
 for (let button of buttons) {
     button.addEventListener('click', function(){
-        console.log('Clicked!!!')
+        button.style.backgroundColor = makeRandColor();
     })
 }
 
